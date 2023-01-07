c3264672bd262280ae9ee3164b6b3799393da601
Linkan Sahu <sahulinkan7@gmail.com>
2023-01-07 09:21:18 +0530

    upated data transformation code

--- demo.py
+++ demo.py
@@ -3,13 +3,24 @@ from housing.logger import logging
 from housing.exception import HousingException
 import os,sys
 from housing.config.configuration import Configuration
+from housing.component.data_transformation import DataTransformation
+
 def main():
     try:
-        # pipeline=Pipeline()
-        # pipeline.run_pipeline()
+        pipeline=Pipeline()
+        pipeline.run_pipeline()
 
-        data_transformation_config=Configuration().get_data_transformation_config()
-        print(data_transformation_config)
+        # data_transformation_config=Configuration().get_data_transformation_config()
+        # print(data_transformation_config)
+
+        # schema_file_path=r"E:\My new World\Ineuron\ML Projects\ml_housing_project\config\schema.yaml"
+        # file_path=r"E:\My new World\Ineuron\ML Projects\ml_housing_project\housing\artifact\data_ingestion\23-01-05-08-38-45\ingested_data\train\housing.csv"
+
+        # df=DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
+
+
     
     except Exception as e:
         logging.error(f"{e}")
